69fd810df5cdd5d74b8517387e963a5af2acbe0d
bandademan <aydlettm@yahoo.com>
2023-01-07 17:11:42 -0500

    Setup all the code
    Time to debug

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(LiveSplitMod.LiveSplitMod), "LiveSplit Mod", "1.0.0", "Mr. Monocle")]
